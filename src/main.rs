5ed7e28af6bdcc42f4611e3f8d8bd8fa5545e523
hellojebus <dev@hellojeb.us>
2019-05-01 21:50:42 -0700

    replace with actual hello world

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
-fn main() {
-	let stdout = stdout();
-	let out = b"Hello fellow Rustacians!";
-	let width = 24;
-
-	let mut writer = BufWriter::new(stdout.lock());
-	say(out, width, &mut writer).unwrap();
+fn main(){
+    println!("Hello, world!");
 }
